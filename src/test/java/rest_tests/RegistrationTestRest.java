bedf8731afa22aa14d745bb09ae592791844ca42
RimmaOgulchanskaia used for github <rimmaogulchanskaya@gmail.com used for github>
2025-07-03 21:05:07 +0300

    Create project

--- src/test/java/rest_tests/RegistrationTestRest.java
+++ src/test/java/rest_tests/RegistrationTestRest.java
@@ -1,4 +1,50 @@
 package rest_tests;
 
-public class RegistrationTestRest {
+import api_rest.AutheniticationController;
+import dto.ErrorMessageDtoString;
+import dto.RegistrationBodyDto;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.Random;
+
+public class RegistrationTestRest
+        extends AutheniticationController {
+
+SoftAssert softAssert= new SoftAssert();
+
+    @Test
+    public void registrationPositiveTest(){
+        int i= new Random().nextInt(1000);
+        RegistrationBodyDto user = RegistrationBodyDto.builder()
+                .username("Modern"+i+"@mail.com")
+                .password("Asdfg1234!")
+                .firstName("Modern")
+                .lastName("Klon")
+                .build();
+        Assert.assertEquals
+                (registrationLogin(user, REGISTRATION_URL).getStatusCode(), 200);
+
+    }
+
+    @Test
+    public void registrationNegativeTest_WrongEmail(){
+        int i= new Random().nextInt(1000);
+        RegistrationBodyDto user = RegistrationBodyDto.builder()
+                .username("Modern"+i+"mail.com")
+                .password("Asdfg1234!")
+                .firstName("Modern")
+                .lastName("Klon")
+                .build();
+//        Assert.assertEquals
+//                (registrationLogin(user, REGISTRATION_URL).getStatusCode(), 400);
+        Response response = registrationLogin(user, REGISTRATION_URL);
+        softAssert.assertEquals(response.getStatusCode(), 400, "validate status code");
+        ErrorMessageDtoString errorMessageDtoString= response.getBody().as(ErrorMessageDtoString.class);
+        softAssert.assertEquals(errorMessageDtoString.getError(), "Bad Request", "validate error");
+        softAssert.assertAll();
+
+    }
 }
