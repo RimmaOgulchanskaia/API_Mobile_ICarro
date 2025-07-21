c0f97834f153df7eef8fb67d3011ed49ed75a386
RimmaOgulchanskaia used for github <rimmaogulchanskaya@gmail.com used for github>
2025-07-21 21:01:10 +0300

    ILCarroMobileTests

---
+++ src/main/java/config/AppiumConfig.java
@@ -0,0 +1,39 @@
+package config;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppiumConfig {
+    public static AppiumDriver<AndroidElement> driver;
+
+
+    @BeforeMethod
+    public void setup(){
+        DesiredCapabilities desiredCapabilities= new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("deviceName", "Small Phone");
+        desiredCapabilities.setCapability("platformVersion", "15");
+        desiredCapabilities.setCapability("appPackage", "com.telran.ilcarro");
+        desiredCapabilities.setCapability("appActivity", ".SplashActivity");
+
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+
+        String url= "http://localhost:4723/wd/hub";
+        try {
+            driver= new AppiumDriver<>(new URL(url), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
